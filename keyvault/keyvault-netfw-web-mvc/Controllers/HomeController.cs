5d00c805bac59b95176318c660d636ee01638436
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-05-18 16:22:24 -0600

    ejemplo

---
+++ keyvault/keyvault-netfw-web-mvc/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace keyvault_netfw_web_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.SecretNameKeyVault = ConfigurationManager.AppSettings["SecretNameKeyVault"];
+            ViewBag.SecretNameUserSecrets = ConfigurationManager.AppSettings["SecretNameUserSecrets"];
+            ViewBag.SecretNameAppSettings = ConfigurationManager.AppSettings["SecretNameAppSettings"];
+
+            return View();
+        }
+    }
+}
