45ccdddf9bdf3c151b7d8187b573f0a407c6b00d
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-08-03 13:18:46 -0600

    ejemplo

---
+++ application-service/NetWebJobConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NetWebJobConsole
+{
+    internal class Program
+    {
+        //https://github.com/Azure/azure-webjobs-sdk/wiki
+
+        static async Task Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(500);
+
+                Console.WriteLine($"Hello, World {i + 1}!");
+            }
+
+            Console.WriteLine($"finish");
+
+            if (new Random().Next(0, 2) == 1)
+            {
+                Console.WriteLine("STATUS : Failed");
+
+                throw new Exception("error in webjob");
+            }
+            else
+            {
+                Console.WriteLine("STATUS : Success");
+            }
+        }
+    }
+}
