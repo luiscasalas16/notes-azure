006f4c4966074cd9904a7c9eafddc29c08457f81
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-08-24 22:16:07 -0600

    ejemplo

--- application-gateway/NetWafWebMvc/Controllers/TestMvcController.cs
+++ application-gateway/NetWafWebMvc/Controllers/TestMvcController.cs
@@ -1,10 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System.Text;
 
 namespace NetWafWebMvc.Controllers
 {
     public class TestMvcController : Controller
     {
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            for (int i = 0; i < context.ActionArguments.Count; i++)
+            {
+                string key = context.ActionArguments.Keys.ElementAt(i);
+                string? value = context.ActionArguments[key] as string;
+
+                if (value != null && value.StartsWith("base64|"))
+                {
+                    context.ActionArguments[key] = decode(value.Substring(7, value.Length - 7));
+                }
+            }
+
+            return base.OnActionExecutionAsync(context, next);
+        }
+
         [HttpGet]
         public ActionResult NoParametersGet()
         {
