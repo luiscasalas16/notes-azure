979633abcf256ecba37271e8b5735cb89167885b
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2024-04-15 07:59:38 -0600

    ejemplo

--- static-web-app/examples/angular-static-web-app/src/app/app.component.ts
+++ static-web-app/examples/angular-static-web-app/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-static-web-app';
+
+  private apiUrl: string = 'http://localhost:5000/api';
+
+  public results: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getApi() {
+    this.http.get<any>(`${this.apiUrl}/weatherforecast`).subscribe((resp) => {
+      console.log(resp);
+      this.results = resp;
+    });
+  }
 }
---
+++ static-web-app/examples/net-static-web-app/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.OpenApi;
+
+namespace net_static_web_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //CORS
+            builder.Services.AddCors();
+
+            var app = builder.Build();
+
+            // Swagger
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //CORS
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.MapGet("/", () => "net-static-web-app");
+
+            var summaries = new[]
+            {
+                "Freezing",
+                "Bracing",
+                "Chilly",
+                "Cool",
+                "Mild",
+                "Warm",
+                "Balmy",
+                "Hot",
+                "Sweltering",
+                "Scorching"
+            };
+
+            app.MapGet(
+                    "/api/weatherforecast",
+                    () =>
+                    {
+                        return Enumerable
+                            .Range(1, 5)
+                            .Select(index => new WeatherForecast(
+                                Date: DateTime.Now.AddDays(index),
+                                Temperature: Random.Shared.Next(-20, 55),
+                                Summary: summaries[Random.Shared.Next(summaries.Length)]
+                            ))
+                            .ToArray();
+                    }
+                )
+                .WithName("GetWeatherForecast")
+                .WithOpenApi();
+
+            app.Run();
+        }
+
+        record WeatherForecast(DateTime Date, int Temperature, string? Summary);
+    }
+}
