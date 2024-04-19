e85d034cf936f03b2aae1c138706382d8ec13390
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2024-04-19 13:12:26 -0600

    ejemplo

--- static-web-app/examples/react-static-web-app/src/App.tsx
+++ static-web-app/examples/react-static-web-app/src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [results, setResults] = useState("[]");
+
+  //develpment
+  //const apiUrl: string = "http://localhost:5000/api";
+  //production app service not linked to static web app
+  //const apiUrl: string = 'https://lcs16-swa-as.azurewebsites.net/api';
+  //production app service linked to static web app
+  const apiUrl: string = "/api";
+
+  const getApi = async () => {
+    const endpointUrl = `${apiUrl}/weatherforecast`;
+    console.log(endpointUrl);
+    const response = await fetch(endpointUrl);
+    const data = await response.json();
+    setResults(JSON.stringify(data, null, 4));
+  };
+
   return (
     <div className="content" role="main">
       <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
         <title>React Logo</title>
         <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
-        <g stroke="#61dafb" stroke-width="1" fill="none">
+        <g stroke="#61dafb" strokeWidth="1" fill="none">
           <ellipse rx="11" ry="4.2" />
           <ellipse rx="11" ry="4.2" transform="rotate(60)" />
           <ellipse rx="11" ry="4.2" transform="rotate(120)" />
@@ -14,6 +33,12 @@ function App() {
       </svg>
       <h1>react-static-web-app</h1>
       <p>Test Application</p>
+      <br />
+      <button onClick={getApi} className="btn btn-primary">
+        Get Api
+      </button>
+      <br />
+      <pre>{results}</pre>
     </div>
   );
 }
