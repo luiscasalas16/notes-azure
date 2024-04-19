a392a59ad7726df5e0802792c3f6c7e100c38739
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2024-04-18 20:23:17 -0600

    ejemplo

---
+++ container-apps/NetContainerAppsWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetContainerAppsWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Tests()
+        {
+            return Json(new { result = "ok" });
+        }
+    }
+}
